390553b4942600ae3e52ad63ce511ea63b0de483
Dilbutt <dylanmabinogion@gmail.com>
2019-09-24 21:12:04 -0700

    2.3 commit 2

---
+++ test score.cs
@@ -0,0 +1,17 @@
+//Dylan Cox
+//9/24/19
+//Take test scores until a score of -1 is reached and then find the mean
+using System;
+namespace Loop
+{
+  class WhileLoop
+  {
+    public static void Main(string[]args){
+      score = int.Parse(Console.ReadLine());
+      mean = 0;
+      while (score>-1){
+        for(x=1; x<3; x--){
+          mean = score * x / mean
+        score = int.Parse(Console.ReadLine())
+        }
+      }
