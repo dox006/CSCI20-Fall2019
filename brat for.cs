f8e5d8d8fa81e90419923f95d26272a29140e060
Dilbutt <dylanmabinogion@gmail.com>
2019-09-26 16:18:30 -0700

    commit 2.4.1

---
+++ brat for.cs
@@ -0,0 +1,19 @@
+//Dylan Cox
+//9/26/19
+//Use for loop to repeat string given number of times
+using System;
+namespace Loop
+{
+  class WhileLoop
+  {
+    public static void Main(string[]args)
+    {
+      int i
+      int age=int.Parse(Console.ReadLine("How old is your child?"))
+      for (i=1 ; a<=age ; i++) //define i, define i's end value, define i mutation
+      {
+        Console.WriteLine("Are we there yet?");
+      }
+    }
+  }
+}
