0fd948a91cd024aea2805cc6d19b47cab26fc19d
Dilbutt <dylanmabinogion@gmail.com>
2019-09-26 16:25:09 -0700

    commit 2.4.2

---
+++ 99bottles.cs
@@ -0,0 +1,19 @@
+//Dylan Cox
+//9/26/19
+//Use for loop to repeat string given number of times
+using System;
+namespace Loop
+{
+  class ForLoop
+  {
+    public static void Main(string[]args)
+    {
+      int i
+      int bottles=int.Parse(Console.ReadLine("How many lines do you want to sing?"))
+      for (i=1 ; a>=bottles ; i--) //define i, define i's end value, define i mutation, this time declining
+      {
+        Console.Write({bottles}+"bottles of beer on the wall"+{bottles}"of beer, take one down pass it around");
+      }
+    }
+  }
+}
