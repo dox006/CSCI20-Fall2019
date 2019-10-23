b7a1f5b5f91474aa4a4a5a49da0713a51c24a88a
Dilbutt <dylanmabinogion@gmail.com>
2019-10-22 20:23:03 -0700

    commit 2.3 part 1

---
+++ eggs.cs
@@ -0,0 +1,22 @@
+//Dylan Cox
+//October 22 2019
+//Write a function that takes an integer parameter called dozens and outputs how many eggs are in those dozens.
+using System;
+class MethodDemo
+{
+  static int doit ()
+  {
+    int fullnumber;
+    fullnumber=Console.WriteLine(dozens*12);
+  }
+
+  public static void Main ()
+  {
+   Console.WriteLine("How many egg cartons would you like?");
+   int dozens;
+   dozens=Console.ReadLine();
+   Console.WriteLine("The total number of eggs in these cartons is: ");
+   doit();
+   }
+
+  }
