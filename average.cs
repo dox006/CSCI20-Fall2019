069e93a2411b81e60dcfca7a085c0554bf9e3e87
Dilbutt <dylanmabinogion@gmail.com>
2019-10-24 15:10:05 -0700

    commit 2.3 part 2

---
+++ average.cs
@@ -0,0 +1,17 @@
+
+//Dylan Cox
+//October 22 2019
+//Write a function that takes an integer parameter called dozens and outputs how many eggs are in those dozens.
+using System;
+class MethodDemo
+{
+static int average (int x, int y, int z)
+{
+ (x*y*z)/3
+ }
+static public int compute (average)
+x=Console.ReadLine();
+y=Console.ReadLine();
+z=Console.ReadLine();
+compute()
+}
