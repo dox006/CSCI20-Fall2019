39750b6a21bbc7699201602d3f61406453a0715c
Dilbutt <dylanmabinogion@gmail.com>
2019-10-29 20:25:53 -0700

    commit 3.3

---
+++ average return.cs
@@ -0,0 +1,21 @@
+//Dylan Cox
+//October 29 2019
+//Write a function that takes an integer parameter called dozens and outputs how many eggs are in those dozens, using return in the method
+using System;
+class MethodDemo
+{
+static double average (int x, int y, int z)
+{
+
+ double doit=(x*y*z)/3;
+ return average;
+ }
+static public int compute (average){
+
+x=Console.ReadLine();
+y=Console.ReadLine();
+z=Console.ReadLine();
+compute()
+}
+
+}
