ca6399dc2ca2f4f340b41841409f0ebffa248bcd
Dilbutt <dylanmabinogion@gmail.com>
2019-11-19 11:37:55 -0800

    commit shit

---
+++ triangle.cs
@@ -0,0 +1,25 @@
+//Dylan Cox
+//Nov 19
+//Use methods and appropriate accessors to make triangle
+using System;
+
+class Triangle
+{
+private int side1;
+private int side2;
+private int side3;
+
+
+public Triangle (int side1, int side2, int side3)
+{
+  side1 = inSide1;
+  side2 = inSide2;
+  side3 = inSide3;
+}
+}
+
+public static void Main()
+{
+  Triangle myTriangle;
+  myTriangle = new Triangle(60, 90, 30);
+}
