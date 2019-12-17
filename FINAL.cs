11a12cb4f317a72b0688ea569e98493cfead8764
Dilbutt <dylanmabinogion@gmail.com>
2019-12-17 14:17:59 -0800

    commit final

---
+++ FINAL.cs
@@ -0,0 +1,30 @@
+//Dylan Cox
+//Last edited Dec 15
+//Goal of the project is to create a battle simulator with File I/O, arrays, and loops
+using System;
+[System.Runtime.InteropServices.ComVisible(true)]
+[System.Serializable]
+class Stand
+{
+  private int Power;
+  private int Speed;
+  private int Range;
+  private int Con;
+  private int Dex;
+  private int Growth;
+
+  public Stand (int Power, int Speed, int Range, int Con, int Dex, int Growth)
+  {
+    Power = inPower;
+    Speed = inSpeed;
+    Range = inRange;
+    Con = inCon;
+    Dex = inDex;
+    Growth = inGrowth;
+  }
+}
+public static void Main()
+{
+  Stand StarPlatinum;
+  StarPlatinum = new Stand
+}
